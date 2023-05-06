4c264f5fb6d95f316b5152c0295daa4075f98dc1
Pac12Run8000 <gangstarr78992@>
2023-05-05 19:17:41 -0700

    Initial Commit

---
+++ EntityAndNetworkManager/EntityAndNetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntityAndNetworkManagerApp.swift
+//  EntityAndNetworkManager
+//
+//  Created by Michelle Grover on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EntityAndNetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
